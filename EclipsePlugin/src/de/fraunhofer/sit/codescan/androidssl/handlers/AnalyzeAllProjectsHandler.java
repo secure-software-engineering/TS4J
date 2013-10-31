349faaeb8499fd3072c0bc412055b928aadb4da7
Eric Bodden <eric@bodden.de>
2013-10-31 18:52:48 +0100

    search-filtered analysis is working; now still missing reporting of errors found

--- EclipsePlugin/src/de/fraunhofer/sit/codescan/androidssl/handlers/AnalyzeAllProjectsHandler.java
+++ EclipsePlugin/src/de/fraunhofer/sit/codescan/androidssl/handlers/AnalyzeAllProjectsHandler.java
@@ -6,10 +6,11 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -17,6 +18,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -24,8 +26,18 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.ITypeRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchParticipant;
+import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.SearchRequestor;
 
 import com.google.common.base.Joiner;
 
@@ -38,7 +50,9 @@ import de.ecspride.sslanalysis.Main;
  */
 public class AnalyzeAllProjectsHandler extends AbstractHandler {
 	
-	public static final String ANDROID_NATURE_ID = "com.android.ide.eclipse.adt.AndroidNature";  
+	public static final String ANDROID_NATURE_ID = "com.android.ide.eclipse.adt.AndroidNature";
+	
+	protected Set<IMethod> callBacksFound = new HashSet<IMethod>();
 	
 	/**
 	 * The constructor.
@@ -51,29 +65,61 @@ public class AnalyzeAllProjectsHandler extends AbstractHandler {
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
 		
+		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+
+		ArrayList<IJavaProject> androidProjects = new ArrayList<IJavaProject>();
 		for (IProject p : projects) {
 			if(isAndroidProject(p)) {
 				IJavaProject javaProject = JavaCore.create(p);
-				Collection<IPath> outDirs = getOutputDirectories(javaProject);
-				if(outDirs.isEmpty()) continue;
-				String sootClasspath = getSootClasspath(javaProject);
-				callAnalysis(outDirs,sootClasspath);
+				androidProjects.add(javaProject);
 			}
 		}
+		IJavaProject[] androidProjectArray = androidProjects.toArray(new IJavaProject[0]);
 		
+		SearchEngine searchEngine = new SearchEngine();
+		IJavaSearchScope searchScope = SearchEngine.createJavaSearchScope(androidProjectArray);
+		
+		SearchPattern pattern = SearchPattern.createPattern("onReceivedSslError", IJavaSearchConstants.METHOD, IJavaSearchConstants.DECLARATIONS, SearchPattern.R_CAMELCASE_MATCH | SearchPattern.R_ERASURE_MATCH);
+		SearchRequestor requestor = new SearchRequestor() {
+			public void acceptSearchMatch(SearchMatch match) throws CoreException {
+				IMethod found = (IMethod) match.getElement();
+				callBacksFound.add(found);
+			}
+		};
+		try {
+			searchEngine.search(pattern, new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() }, searchScope, requestor, null);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		
+		Map<IJavaProject,Set<ITypeRoot>> projectToFoundMethods = new HashMap<IJavaProject, Set<ITypeRoot>>();
+		for(IMethod m : callBacksFound) {
+			IJavaProject javaProject = m.getJavaProject();
+			ITypeRoot topLevelType = m.getTypeRoot();
+			Set<ITypeRoot> topLevelTypesToAnalyze = projectToFoundMethods.get(javaProject);
+			if(topLevelTypesToAnalyze==null) topLevelTypesToAnalyze = new HashSet<ITypeRoot>();
+			topLevelTypesToAnalyze.add(topLevelType);
+			projectToFoundMethods.put(javaProject, topLevelTypesToAnalyze);
+		}
+		
+		for(Map.Entry<IJavaProject, Set<ITypeRoot>> entry: projectToFoundMethods.entrySet()) {
+			IJavaProject project = entry.getKey();
+			Set<ITypeRoot> topLevelTypesToAnalyze = entry.getValue();
+			callAnalysis(topLevelTypesToAnalyze, getSootClasspath(project));
+		}
 		
 		return null;
 	}
 
-	private void callAnalysis(Collection<IPath> outDirs, String sootClasspath) {
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		Set<String> processDirs = new HashSet<String>();
-		for (IPath outDir : outDirs) {
-			processDirs.add("-process-dir "+root.findMember(outDir).getLocation().toString());
+	private void callAnalysis(Set<ITypeRoot> topLevelTypesToAnalyze, String sootClasspath) {
+		Set<String> applicationClasses = new HashSet<String>();
+		for (ITypeRoot typeRoot : topLevelTypesToAnalyze) {
+			IType type = typeRoot.findPrimaryType();
+			String qualifiedName = type.getFullyQualifiedName();
+			applicationClasses.add(qualifiedName);
 		}
-		String[] args = ("-f none -p cg all-reachable:true -no-bodies-for-excluded -w -pp -cp "+sootClasspath+" "+Joiner.on(" ").join(processDirs)).split(" ");
+		String[] args = ("-f none -p cg all-reachable:true -no-bodies-for-excluded -w -pp -cp "+sootClasspath+" "+Joiner.on(" ").join(applicationClasses)).split(" ");
 		System.err.println(Joiner.on(" ").join(args));
 		Main.main(args);
 	}
