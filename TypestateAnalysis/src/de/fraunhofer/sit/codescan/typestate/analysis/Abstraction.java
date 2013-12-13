4712d3cbb2167de8b63d8969688bacc26d2c6ddb
Eric Bodden <eric@bodden.de>
2013-12-13 17:48:42 +0100

    started implementing typestate analysis

--- TypestateAnalysis/src/de/fraunhofer/sit/codescan/typestate/analysis/Abstraction.java
+++ TypestateAnalysis/src/de/fraunhofer/sit/codescan/typestate/analysis/Abstraction.java
@@ -1,5 +1,105 @@
 package de.fraunhofer.sit.codescan.typestate.analysis;
 
-public class Abstraction {
+import java.util.HashSet;
 
+import soot.Unit;
+import soot.Value;
+import soot.jimple.AssignStmt;
+
+public class Abstraction implements Cloneable {
+	
+	protected Unit constrCallToValueGroup;
+	protected Value valueGroup;
+	protected HashSet<Value> elements = new HashSet<Value>();
+	protected boolean modelValueAdded;
+	
+	public final static Abstraction ZERO = new Abstraction(); 
+	
+	public Abstraction(AssignStmt constrCallToValueGroup) {
+		this.constrCallToValueGroup = constrCallToValueGroup;
+		this.valueGroup = (Value) constrCallToValueGroup.getLeftOp();
+		modelValueAdded = false;
+	}
+	
+	public Value getValueGroupLocal() {
+		return valueGroup;
+	}
+	
+	private Abstraction() {		
+	}
+	
+	public Abstraction derive(Value newLocal) {
+		Abstraction copy = copy();
+		copy.valueGroup = newLocal;
+		return copy;
+	}
+	
+	public Abstraction markedAsTainted() {
+		Abstraction copy = copy();
+		copy.modelValueAdded = true;
+		return copy;
+	}
+		
+	public Abstraction markedAsFlushed() {
+		Abstraction copy = copy();
+		copy.modelValueAdded = false;
+		return copy;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result
+				+ ((constrCallToValueGroup == null) ? 0
+						: constrCallToValueGroup.hashCode());
+		result = prime * result
+				+ ((elements == null) ? 0 : elements.hashCode());
+		result = prime * result + (modelValueAdded ? 1231 : 1237);
+		result = prime * result
+				+ ((valueGroup == null) ? 0 : valueGroup.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Abstraction other = (Abstraction) obj;
+		if (constrCallToValueGroup == null) {
+			if (other.constrCallToValueGroup != null)
+				return false;
+		} else if (!constrCallToValueGroup.equals(other.constrCallToValueGroup))
+			return false;
+		if (elements == null) {
+			if (other.elements != null)
+				return false;
+		} else if (!elements.equals(other.elements))
+			return false;
+		if (modelValueAdded != other.modelValueAdded)
+			return false;
+		if (valueGroup == null) {
+			if (other.valueGroup != null)
+				return false;
+		} else if (!valueGroup.equals(other.valueGroup))
+			return false;
+		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Abstraction copy() {
+		try {
+			Abstraction clone = (Abstraction) super.clone();
+			clone.elements = (HashSet<Value>) elements.clone();
+			return clone;
+		} catch (CloneNotSupportedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 }
--- TypestateAnalysis/src/de/fraunhofer/sit/codescan/typestate/analysis/TypestateAnalysisProblem.java
+++ TypestateAnalysis/src/de/fraunhofer/sit/codescan/typestate/analysis/TypestateAnalysisProblem.java
@@ -1,20 +1,32 @@
 package de.fraunhofer.sit.codescan.typestate.analysis;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import heros.FlowFunction;
 import heros.FlowFunctions;
+import heros.flowfunc.Gen;
+import heros.flowfunc.Identity;
 import soot.SootMethod;
 import soot.Unit;
+import soot.jimple.AssignStmt;
+import soot.jimple.DefinitionStmt;
+import soot.jimple.InvokeExpr;
 import de.fraunhofer.sit.codescan.framework.AbstractIFDSAnalysisProblem;
 import de.fraunhofer.sit.codescan.framework.IFDSAnalysisManager;
 
+@SuppressWarnings("serial")
 public class TypestateAnalysisProblem extends AbstractIFDSAnalysisProblem<Abstraction> {
+
+	private static final String VALUE_GROUP_CONSTRUCTOR_SIG = "<example1.ValueGroup: void <init>()>";
 
 	public TypestateAnalysisProblem(IFDSAnalysisManager manager) {
 		super(manager);
 	}
 
 	protected Abstraction createZeroValue() {
-		return new Abstraction();
+		return Abstraction.ZERO;
 	}
 
 	@Override
@@ -22,19 +34,45 @@ public class TypestateAnalysisProblem extends AbstractIFDSAnalysisProblem<Abstra
 		return new FlowFunctions<Unit, Abstraction, SootMethod>() {
 
 			public FlowFunction<Abstraction> getCallFlowFunction(Unit src, SootMethod dest) {
-				return null;
+				return Identity.v();
 			}
 
 			public FlowFunction<Abstraction> getCallToReturnFlowFunction(Unit curr, Unit succ) {
-				return null;
+				if(curr instanceof AssignStmt) {
+					AssignStmt assign = (AssignStmt) curr;
+					if(assign.containsInvokeExpr()) {
+						InvokeExpr ie = assign.getInvokeExpr();
+						if(ie.getMethodRef().getSignature().equals(VALUE_GROUP_CONSTRUCTOR_SIG)) {
+							return new Gen<Abstraction>(new Abstraction(assign), zeroValue());
+						}
+					}
+				}
+				//TODO handle adding of ModelValue to the ValueGroup etc.
+				return Identity.v();
 			}
 
 			public FlowFunction<Abstraction> getNormalFlowFunction(Unit curr,Unit succ) {
-				return null;
+				if(curr instanceof DefinitionStmt) {
+					final DefinitionStmt assign = (DefinitionStmt) curr;
+					return new FlowFunction<Abstraction>() {
+
+						public Set<Abstraction> computeTargets(final Abstraction source) {
+							//TODO replace also element locals
+							if(source.getValueGroupLocal().equals(assign.getRightOp())) {
+								return new HashSet<Abstraction>() {{
+									add(source);
+									add(source.derive(assign.getLeftOp()));
+								}};
+							} else
+								return Collections.singleton(source);
+						}
+					};
+				}
+				return Identity.v();
 			}
 
 			public FlowFunction<Abstraction> getReturnFlowFunction(Unit callSite, SootMethod callee, Unit exitStmt, Unit retSite) {
-				return null;
+				return Identity.v();
 			}
 		};
 	}
