64b667f71051acb2a16addd34bc8c152051a03fe
Eric Bodden <eric@bodden.de>
2013-12-16 11:44:13 +0100

    equals/hashCode for ErrorMarker

--- SootBridge/src/de/fraunhofer/sit/codescan/sootbridge/ErrorMarker.java
+++ SootBridge/src/de/fraunhofer/sit/codescan/sootbridge/ErrorMarker.java
@@ -24,4 +24,45 @@ public class ErrorMarker {
 		return lineNumber;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((className == null) ? 0 : className.hashCode());
+		result = prime * result
+				+ ((errorMessage == null) ? 0 : errorMessage.hashCode());
+		result = prime * result + lineNumber;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ErrorMarker other = (ErrorMarker) obj;
+		if (className == null) {
+			if (other.className != null)
+				return false;
+		} else if (!className.equals(other.className))
+			return false;
+		if (errorMessage == null) {
+			if (other.errorMessage != null)
+				return false;
+		} else if (!errorMessage.equals(other.errorMessage))
+			return false;
+		if (lineNumber != other.lineNumber)
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return className+":"+lineNumber+" "+errorMessage;
+	}
+
 }
