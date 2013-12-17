4ea20dfb6941578b38239777c202f0a63c9d5227
Eric Bodden <eric@bodden.de>
2013-12-17 19:39:04 +0100

    removed unused import

--- EclipseFrameworkPlugin/src/de/fraunhofer/sit/codescan/framework/internal/handlers/StartupHandler.java
+++ EclipseFrameworkPlugin/src/de/fraunhofer/sit/codescan/framework/internal/handlers/StartupHandler.java
@@ -3,7 +3,6 @@ package de.fraunhofer.sit.codescan.framework.internal.handlers;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
