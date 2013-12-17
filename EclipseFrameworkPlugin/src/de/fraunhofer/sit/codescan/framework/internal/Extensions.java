e878fdad5bb2bafde624c837b37e5ffd5f464161
Eric Bodden <eric@bodden.de>
2013-12-17 18:38:35 +0100

    removed unused code

--- EclipseFrameworkPlugin/src/de/fraunhofer/sit/codescan/framework/internal/Extensions.java
+++ EclipseFrameworkPlugin/src/de/fraunhofer/sit/codescan/framework/internal/Extensions.java
@@ -1,18 +1,10 @@
 package de.fraunhofer.sit.codescan.framework.internal;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import de.fraunhofer.sit.codescan.framework.IIFDSAnalysisPlugin;
-import de.fraunhofer.sit.codescan.framework.IMethodBasedAnalysisPlugin;
 
 public class Extensions {
-
-	private final static Logger LOGGER = LoggerFactory.getLogger(Extensions.class);
 
 	/**
 	 * Returns the set of all registered analysis plugins. 
@@ -21,36 +13,6 @@ public class Extensions {
 	    IExtensionRegistry reg = Platform.getExtensionRegistry();
 	    IConfigurationElement[] elements = reg.getConfigurationElementsFor(Constants.EXTENSION_POINT_ID);
 	    return elements;
-	}
-	
-	public static IIFDSAnalysisPlugin[] createIFDSAnalysisPluginObjects(IConfigurationElement extension) {
-		try {
-			IConfigurationElement[] children = extension.getChildren("ifdsAnalysis");
-			IIFDSAnalysisPlugin[] res = new IIFDSAnalysisPlugin[children.length];
-			int i=0;
-			for (IConfigurationElement analysisConfig : children) {
-				res[i++] = (IIFDSAnalysisPlugin) analysisConfig.createExecutableExtension("class"); 
-			}
-			return res;
-		} catch (CoreException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static IMethodBasedAnalysisPlugin[] createMethodBasedAnalysisPluginObjects(IConfigurationElement extension) {
-		try {
-			IConfigurationElement[] children = extension.getChildren("methodBasedAnalysis");
-			IMethodBasedAnalysisPlugin[] res = new IMethodBasedAnalysisPlugin[children.length];
-			int i=0;
-			for (IConfigurationElement analysisConfig : children) {
-				res[i++] = (IMethodBasedAnalysisPlugin) analysisConfig.createExecutableExtension("class"); 
-			}
-			return res;
-		} catch (CoreException e) {
-			LOGGER.debug("",e);
-			return null;
-		}
 	}
 
 }
